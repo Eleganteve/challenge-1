8f20afd2f9dc7472c8bf37db4096958c20de449c
Eleganteve <36538261+Eleganteve@users.noreply.github.com>
2018-10-10 15:45:14 -0400

    Add files via upload

---
+++ website.js
@@ -0,0 +1,101 @@
+</style>
+<link rel='stylesheet' id='Total_Soft_Cal-css'  href='https://store manager.or.ke/wp-content/plugins/calendar-event/CSS/Total-Soft-Calendar-Widget.css?ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='fontawesome-css-css'  href='https://store manager.or.ke/wp-content/plugins/calendar-event/CSS/totalsoft.css?ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='contact-form-7-css'  href='https://store manager.or.ke/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='social-widget-css'  href='https://store manager.or.ke/wp-content/plugins/social-media-widget/social_widget.css?ver=4.9.8' type='text/css' media='all' /><link rel='stylesheet' id='SFSIPLUSmainCss-css'  href='https://fisauon.or.ke/wp-content/plugins/ultimate-social-media-plus/css/sfsi-style.css?ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='google-fonts-css'  href='https://fonts.googleapis.com/css?family=Noto+Sans%3A400%2C400i%2C700%2C700i%7CLato%3A300%2C300i%2C400%2C400i%2C700%2C700i%2C900%2C900i%7CPoppins%3A300%2C400%2C500%2C600%2C700&#038;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='animate-css'  href='https://store manager.or.ke/wp-content/themes/business-eye/assets/frameworks/wow/css/animate.min.css?ver=3.4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-mmenu-all-css'  href='https://store manager.or.ke/wp-content/themes/business-eye/assets/frameworks/mmenu/css/jquery.mmenu.all.css?ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-photobox-css'  href='https://store manager.or.ke/wp-content/themes/business-eye/assets/frameworks/photobox/photobox.css?ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-slick-css'  href='https://store manager.or.ke/wp-content/themes/business-eye/assets/frameworks/slick/slick.css?ver=3.4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='business-eye-style-css'  href='https://store manager.or.ke/wp-content/themes/business-eye/style.css?ver=4.9.8' type='text/css' media='all' />
+<script type='text/javascript' src='https://store manager.or.ke/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='https://store manager.or.ke/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='https://store manager.or.ke/wp-includes/js/jquery/ui/core.min.js?ver=1.11.4'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var object = {"ajaxurl":"https:\/\/.or.ke\/wp-admin\/admin-ajax.php"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://store manager.or.ke/wp-content/plugins/calendar-event/JS/Total-Soft-Calendar-Widget.js?ver=4.9.8'></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" crossorigin="anonymous"></script><script type='text/javascript' src='https://fisauon.or.ke/wp-content/themes/business-eye/assets/frameworks/mmenu/js/jquery.mmenu.min.all.js?ver=4.7.5'></script>
+<link rel='https://api.w.org/' href='https://store manager.or.ke/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://store manager.or.ke/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://store manager.or.ke/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.9.8" />
+<link rel="canonical" href="https://store manager.or.ke/" />
+<link rel='shortlink' href='https://store manager.or.ke/' />
+		<!-- Custom Logo: hide header text -->
+		<style id="custom-logo-css" type="text/css">
+			.site-title, .site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
+		</style>
+	<link rel="alternate" type="application/json+oembed" href="https://storemanager.or.ke/wp-json/oembed/1.0/embed?url=https%3A%2F%2Ffisauon.or.ke%2F" />
+<link rel="alternate" type="text/xml+oembed" href="https://store manager.or.ke/wp-json/oembed/1.0/embed?url=https%3A%2F%2Ffisauon.or.ke%2F&#038;format=xml" />
+<script type='text/javascript'>
+							SG_POPUPS_QUEUE = [];
+							git log	def create_user(self):
+		user = {}
+		user["id"] = users[-1]["id"] + 1
+		user["name"] = self.name
+		user["email"] = self.email
+		user["password"] = self.password
+
+		users.append(user)
+		return users
+
+	def login_user(self, email, password):
+		self.email = email
+		self.password = password
+
+		for user in users:
+			if user["email"] == self.email  and user["password"] == self.password:
+				return "granted", user
+		return "You have entered the wrong details"
+
+
+class Admin(User):
+	def __init__(self, name, email, password):		
+		User.__init__(self, name, email, password)
+
+
+	def delete_user(self, email):
+		self.email = email
+		for user in users:
+			if admin["email"] == self.email:
+				users.remove(user)
+				return users
+		return "user email does not exist"
+		if not admin :
+		return :"it cannot modifythe results."
+	
+	def create_user(self,email):
+			self.email =email
+			if admin[email]==self.email:
+					user.create(product)
+					return products.
+		if admin{email==self.email:
+					user.modify(product)
+                 return products.
+				</script>    	<script>
+			
+            });
+			function Refer to id:(ref) {
+				var feed_id = '';
+				var feedtype = ;
+				var email = jQuery(ref).find('input[name="data[Widget][email]"]').val();
+				if ((email != "Enter your email") && (filter.test(email))) {
+					if (feedtype == "8") {
+						var url = "https://www.specificfeeds.com/widgets/subscribeWidget/"+feed_id+"/"+feedtype;
+						window.open(url, "popupwindow", "scrollbars=yes,width=1080,height=760");
+						return true;
+					}
+				} else {
+					alert("Please enter email address");
+					jQuery(ref).find('input[name="data[Widget][email]"]').focus();
+					return false;
+				}
+			}
+		</script>
